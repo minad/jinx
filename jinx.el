a938b3f7c55f0361bcb2a9ae9a3e29b07b326530
Daniel Mendler <mail@daniel-mendler.de>
2023-04-23 15:01:00 +0200

    :keymap attribute is unnecessary

--- jinx.el
+++ jinx.el
@@ -810,7 +810,6 @@ If prefix argument ALL non-nil correct all misspellings."
   "Enchanted Spell Checker."
   :lighter (:eval (concat " Jinx[" jinx-languages "]"))
   :group 'jinx
-  :keymap jinx-mode-map
   (cond
    (jinx-mode
     (jinx--load-module)
