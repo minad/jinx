9a10a621c505c65acfb6222240263e29ad546f68
Daniel Mendler <mail@daniel-mendler.de>
2023-07-02 09:57:28 +0200

    Version 0.9

--- jinx.el
+++ jinx.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2023
-;; Version: 0.8
+;; Version: 0.9
 ;; Package-Requires: ((emacs "27.1") (compat "29.1.4.0"))
 ;; Homepage: https://github.com/minad/jinx
 ;; Keywords: convenience, wp
