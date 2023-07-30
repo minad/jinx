772fee317f48fa921cc64cd53f22dac11e03e2cb
Daniel Mendler <mail@daniel-mendler.de>
2023-07-30 13:58:25 +0200

    jinx--word-valid-p: More restrictive check of capitalized words

--- jinx.el
+++ jinx.el
@@ -356,11 +356,12 @@ Predicate may return a position to skip forward.")
         case-fold-search)
     (or (member word jinx--session-words)
         ;; Allow capitalized words
-        (and (string-match-p "\\`[[:upper:]]" word)
+        (and (string-match-p "\\`[[:upper:]][[:lower:]]+\\'" word)
              (cl-loop
               for w in jinx--session-words
               thereis (and (eq t (compare-strings word 0 1   w 0 1   t))
-                           (eq t (compare-strings word 1 nil w 1 nil nil)))))
+                           (eq t (compare-strings word 1 nil w 1 nil nil))
+                           (string-match-p "\\`[[:lower:]]+\\'" w))))
         (cl-loop for dict in jinx--dicts
                  thereis (jinx--mod-check dict word)))))
 
