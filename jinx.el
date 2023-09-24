10485e0682fb828eea7fdf3c13ecd64e0ecb0450
Daniel Mendler <mail@daniel-mendler.de>
2023-09-24 09:56:46 +0200

    Update jinx-exclude-regexps

--- jinx.el
+++ jinx.el
@@ -146,7 +146,7 @@ checking."
 (defcustom jinx-exclude-regexps
   '((emacs-lisp-mode "Package-Requires:.*$")
     (t "[A-Z]+\\>"         ;; Uppercase words
-       "-\\>"              ;; Hyphens used as bullet points
+       "-+\\>"             ;; Hyphens used as lines or bullet points
        "\\w*?[0-9]\\w*\\>" ;; Words with numbers, hex codes
        "[a-z]+://\\S-+"    ;; URI
        "<?[-+_.~a-zA-Z][-+_.~:a-zA-Z0-9]*@[-.a-zA-Z0-9]+>?" ;; Email
