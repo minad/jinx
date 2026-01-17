b412673dec759131fe0abb346998b55225fbe771
Daniel Mendler <mail@daniel-mendler.de>
2026-01-17 17:50:00 +0100

    Version 2.6

--- jinx.el
+++ jinx.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2023
-;; Version: 2.5
+;; Version: 2.6
 ;; Package-Requires: ((emacs "29.1") (compat "30"))
 ;; URL: https://github.com/minad/jinx
 ;; Keywords: convenience, text
